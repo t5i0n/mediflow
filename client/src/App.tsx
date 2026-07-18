6ac08ade773d2cc20954509d075a700ffe923a2e
t5i0n <tesfayetsion270@gmail.com>
2026-07-18 19:31:12 +0300

    add book appointment button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "./components/Button";
+
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold text-blue-600">MediFlow</h1>
+      <Button onClick={() => alert("Hello from MediFlow!")}>
+        Book Appointment
+      </Button>
     </div>
   );
 }
