1c27bacf9b6a195d4a42375eeeefda07bbd277db
t5i0n <tesfayetsion270@gmail.com>
2026-07-19 14:08:46 +0300

    Add basic Express server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "MediFlow API is running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
